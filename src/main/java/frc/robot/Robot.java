b7ce93dcecc417c021a7acfea7d292da7010b677
Mr. A <GabrielArtaud@comcast.net>
2026-02-01 10:02:44 -0500

    Copied HubActiveState from CTRE's example

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.utils.HubActiveState;
 
 import java.util.Optional;
 
@@ -30,6 +31,7 @@ public class Robot extends TimedRobot {
 	private Command m_autonomousCommand;
 
 	private RobotContainer m_robotContainer;
+	private final HubActiveState m_hubInstance = HubActiveState.getInstance();
 
 	/**
 	 * This function is run when the robot is first started up and should be used for any
