997257dc6ea9a63e6e3efcf8c7ac16abf304b521
FRC2495 <frc2495@gmail.com>
2026-03-27 16:30:45 -0400

    fixed magic shooter (fixed equation)

--- src/main/java/frc/robot/utils/Magic.java
+++ src/main/java/frc/robot/utils/Magic.java
@@ -13,10 +13,11 @@ public class Magic {
 	{
 		//return 2000 + distance * 500; // 2000 rpm at 0 meter, increases by 500 rpm per meter - TODO: tune this formula
 
-		double distanceInFeet = Units.metersToFeet(distance); // convert meters to feet
+		//double distanceInFeet = Units.metersToFeet(distance); // convert meters to feet
 
-		return 2.6 * Math.pow(distanceInFeet, 2) + 16.8*distanceInFeet + 2371 +500; // Example quadratic formula: RPM = 12.7 * (distance in feet)^2 - 57.7 * (distance in feet) + 2660 - TODO: tune this formula
+		//return 2.6 * Math.pow(distanceInFeet, 2) + 16.8*distanceInFeet + 2371 +500; // Example quadratic formula: RPM = 12.7 * (distance in feet)^2 - 57.7 * (distance in feet) + 2660 - TODO: tune this formula
 
+		return 360 * distance + 1580;
 	}
 }
 
